26f414973ff14b641c6710d453feb916276294a0
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2025-12-15 15:54:52 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,45 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  document.body.classList.add("preloader-active");
-
   const preloader = document.getElementById("preloader");
-  const logoWrapper = document.querySelector(".preloader-logo-wrapper");
-  const textEl = document.getElementById("type-text");
-  const cursor = document.querySelector(".cursor");
-  const textContainer = document.querySelector(".preloader-text");
 
   setTimeout(() => {
-    logoWrapper.classList.add("silver-sweep");
-  }, 400);
+    preloader.style.opacity = "0";
+  }, 1800);
 
-  const text = window.innerWidth <= 600
-    ? "MATTHIAS\nSILBERHAIN"
-    : "MATTHIAS SILBERHAIN";
+  setTimeout(() => {
+    preloader.style.display = "none";
+    document.body.classList.remove("preloader-active");
+  }, 2600);
 
-  let i = 0;
-  const speed = 120;
+  const burger = document.getElementById("burger");
+  const navigation = document.getElementById("navigation");
 
-  function typeWriter() {
-    if (i < text.length) {
-      textEl.innerHTML += text[i] === "\n" ? "<br>" : text[i];
-      i++;
-      setTimeout(typeWriter, speed);
-    } else {
-      cursor.style.display = "none";
-      textContainer.classList.add("silver-glow");
-
-      setTimeout(() => {
-        preloader.style.opacity = "0";
-      }, 1200);
-
-      setTimeout(() => {
-        preloader.style.display = "none";
-        document.body.classList.remove("preloader-active");
-      }, 1800);
-    }
-  }
-
-  setTimeout(typeWriter, 900);
+  burger.addEventListener("click", () => {
+    navigation.classList.toggle("aktiv");
+  });
 
   document.getElementById("jahr").textContent =
     new Date().getFullYear();
