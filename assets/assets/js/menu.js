05ae21fcf1da98c953705e959cdbfc46afe50b2f
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2025-12-15 12:36:29 +0100

    Update menu.js

--- assets/assets/js/menu.js
+++ assets/assets/js/menu.js
@@ -1,4 +1,12 @@
-function menueUmschalten() {
-  const nav = document.querySelector('.hauptnavigation');
-  nav.classList.toggle('offen');
-}
+// Preloader
+window.addEventListener('load', function() {
+  document.body.classList.add('loaded');
+});
+
+// Mobile Navigation Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.hauptnavigation');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
